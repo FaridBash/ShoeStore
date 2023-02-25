0c332fe05e7466cf1856d8ec1446f71da11fbd2c
FaridBash <farid.bashiti@gmail.com>
2023-02-26 00:21:10 +0200

    nopath

--- src/components/NoPath.jsx
+++ src/components/NoPath.jsx
@@ -5,6 +5,6 @@ export default function NoPath(){
 
 
     return <div>
-        <h1 id='myH1'>Don't Try <span id='me'>Me</span>, I did my work</h1>
+        <h1 id='myH1'>Don't Look At <span id='me'>Me</span>, You Didnt Request It</h1>
     </div>
 }
